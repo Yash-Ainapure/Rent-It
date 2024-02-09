9e188fe909094922c194332a5e9f381900abc9c1
Harsh2504 <harsh2504patil@gmail.com>
2024-02-09 14:01:39 +0530

    Login and Signup UI done

--- app/src/main/java/com/example/rent_it/MainActivity.java
+++ app/src/main/java/com/example/rent_it/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rent_it;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,12 +19,16 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button register,login;
+    Button login;
+    TextView register;
     EditText username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         register=findViewById(R.id.register);
         login=findViewById(R.id.login);
--- app/src/main/java/com/example/rent_it/Register.java
+++ app/src/main/java/com/example/rent_it/Register.java
@@ -21,7 +21,9 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         etName=findViewById(R.id.editTextText);
         etEmail = findViewById(R.id.editTextTextEmailAddress);
         etPhone = findViewById(R.id.editTextPhone);
